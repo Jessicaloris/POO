2aeb026a20de55f01e972caee7f80b948dbefc9e
Jessica Loris <dearjess535@gmail.com>
2025-05-06 09:35:44 -0300

    Anotações

---
+++ anotações.py
@@ -0,0 +1,25 @@
+"""
+conceitos de poo:
+
+* atribuição - São as características de um objeto. Ficam dentro da classe e variam de objeto para objeto.
+* métodos - São as ações que o objeto pode fazer. Também ficam dentro da classe.
+* objeto - É uma instância (um exemplo real) da classe. Quando você cria um objeto, está criando algo com base no molde da classe.
+* mensagem - A comunicação entre objetos.
+* classe - É o molde para criar objetos. Define quais atributos (dados) e métodos (ações) o objeto terá.
+* abstração - É esconder detalhes complexos e mostrar só o necessário para quem usa o objeto.
+* encapsulamento - É o conceito de esconder os detalhes internos do objeto e só permitir o acesso necessário. Protege os dados.
+* herança - Permite que uma classe herde características e comportamentos de outra.
+* polimorfismo - Permite que métodos com o mesmo nome façam coisas diferentes dependendo da classe. superclasse|subclasse
+
+* Classes - Uma classe define:
+Quais *atributos* os objetos terão (ex: nome, cor, idade).
+Quais *métodos* os objetos poderão executar (ex: andar, falar, buzinar).
+Pense na classe como o projeto de uma casa. Você pode usar esse projeto para construir várias casas (objetos),
+cada uma com suas cores e móveis (atributos diferentes), mas todas baseadas no mesmo plano.
+Padrão de nomenclatura: Pascal Case - toda primeira letra da cada palavra é sempre maiuscula.
+Metodo construtor em python: __
+ex:
+class NomeClasse:
+def __init__(self):
+*
+"""
